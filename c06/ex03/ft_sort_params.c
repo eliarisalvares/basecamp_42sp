2878d837fe8140c0e3fa9cc38fd888f146084e3f
elraira- <elraira-@student.42sp.org.br>
2021-06-18 22:33:01 +0000

    fixing ex03

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -22,7 +22,7 @@ int 	ft_strcmp(char *s1, char *s2)
 	int	c;
 
 	c = 0;
-	while (s1[c] != '\0' || s2[c] != '\0')
+	while (s1[c] != '\0')
 	{
 		if (s1[c] != s2[c])
 			return (s1[c] - s2[c]);
@@ -40,11 +40,12 @@ int	main(int argc, char *argv[])
 	a = 1;
 	while (a < argc - 1)
 	{
-		if (a < argc || ft_strcmp(argv[a], argv[a + 1]) > 0)
+		if (a < argc && ft_strcmp(argv[a], argv[a + 1]) > 0)
 		{
 			swap = argv[a];
 			argv[a] = argv[a + 1];
 			argv[a + 1] = swap;
+			a = 0;
 		}
 		a++;
 	}
